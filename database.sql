96f846dda0f35a626f017a742b4317a9e3efa0cd
Le creepeur petitè <nikostpt@hotmail.com>
2020-12-27 03:44:18 +0200

    db alpha

---
+++ database.sql
@@ -0,0 +1,9 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "aggelia" (
+	"aggelia_id"	INTEGER,
+	"akinito_id"	INTEGER NOT NULL UNIQUE,
+	"politis_id"	INTEGER NOT NULL UNIQUE,
+	FOREIGN KEY("politis_id") REFERENCES "aggelia",
+	PRIMARY KEY("aggelia_id" AUTOINCREMENT)
+);
+COMMIT;
