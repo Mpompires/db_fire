2d3f902fbfd80481b8472ce3ac435eaa6923c7a8
ignekt <ignekt@gmail.com>
2020-12-30 21:30:56 +0200

    Refactor

--- db.sql
+++ db.sql
@@ -1,105 +1,109 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "akinhto" (
-	"akinito_id"	INTEGER,
-	"surface_area"	REAL,
-	"area"	VARCHAR(255),
-	"area_coords"	VARCHAR(255),
-	"description"	TEXT,
+	"akinito_id" INTEGER,
+	"surface_area" REAL,
+	"area" TEXT,
+	"area_coords" TEXT,	
+	"description" TEXT,
 	"extra"	TEXT,
 	PRIMARY KEY("akinito_id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "melos" (
-	"melos_id"	INTEGER,
-	"username"	VARCHAR(64),
-	"passwork"	VARCHAR(64),
-	"email"	VARCHAR(64),
-	"first_name"	VARCHAR(64),
-	"last_name"	VARCHAR(64),
-	"is_endiaferomenos"	BOOL,
-	"is_pwlhths"	BOOL,
+	"melos_id" INTEGER,
+	"username" TEXT,
+	"passwork" TEXT,	
+	"email"	TEXT,
+	"first_name" TEXT,
+	"last_name"	TEXT,
+	"is_endiaferomenos"	REAL,
+	"is_pwlhths" REAL,
 	PRIMARY KEY("melos_id" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "aggelia" (
-	"aggelia_id"	INTEGER,
-	"akinito_id"	INTEGER,
-	"pwlhths_id"	INTEGER,
+	"aggelia_id" INTEGER,
+	"akinito_id" INTEGER,
+	"pwlhths_id" INTEGER,
 	"price"	REAL,
-	"aggelia_type"	TINYINT(1),
-	"created_on"	DATETIME,
-	"modified_on"	DATETIME,
-	"closed_on"	DATETIME,
-	"available"	BOOL,
-	"available_since"	DATETIME,
+	"aggelia_type" TEXT 
+        CHECK(aggelia_type = "enoikiazetai" or aggelia_type = "pwlhtai"),
+	"created_on" TEXT,
+	"modified_on" TEXT,
+	"closed_on"	TEXT,
+	"available"	REAL,
+	"available_since" TEXT,
 	"text"	TEXT,
 	PRIMARY KEY("aggelia_id" AUTOINCREMENT),
-	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id"),
-	FOREIGN KEY("pwlhths_id") REFERENCES "m_pwlhths"("melos_id")
+	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id") ON DELETE CASCADE,
+	FOREIGN KEY("pwlhths_id") REFERENCES "m_pwlhths"("melos_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "m_endiaferomenos_endiaferetai" (
-	"melos_id"	INTEGER,
-	"aggelia_id"	INTEGER,
+	"melos_id" INTEGER,
+	"aggelia_id" INTEGER,
 	PRIMARY KEY("melos_id","aggelia_id")
+    FOREIGN KEY("melos_id") REFERENCES "melos"("melos_id") ON DELETE CASCADE, 
+    FOREIGN KEY("aggelia_id") REFERENCES "aggelia"("aggelia_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "a_epaggelmatikos_xwros" (
-	"akinito_id"	INTEGER,
-	"parking_spot"	BOOL,
-	"construnction_year"	YEAR,
+	"akinito_id" INTEGER,
+	"parking_spot" REAL,
+	"construnction_year" TEXT,
 	"internal"	TEXT,
 	"external"	TEXT,
 	PRIMARY KEY("akinito_id"),
-	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id")
+	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "a_gh" (
-	"akinito_id"	INTEGER,
-	"building_coeff"	REAL,
-	"external"	TEXT,
+	"akinito_id" INTEGER,
+	"building_coeff" REAL,
+	"external" TEXT,
 	PRIMARY KEY("akinito_id"),
-	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id")
+	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "a_katoikia" (
-	"akinito_id"	INTEGER,
-	"katoikia_type"	TINYINT(1),
-	"heating_system"	TINYINT(1),
+	"akinito_id" INTEGER,
+	"katoikia_type"	TEXT
+        CHECK(katoikia_type = "monokatoikia" or katoikia_type = "polykatoikia"),
+	"heating_system" TEXT
+        CHECK(heating_system = "autonomh" or heating_system = "kentrikh"),
 	"bathrooms"	INTEGER,
 	"floor"	INTEGER,
-	"construction_year"	YEAR,
+	"construction_year"	TEXT,
 	"internal"	TEXT,
 	"external"	TEXT,
 	PRIMARY KEY("akinito_id"),
-	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id")
+	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "idiokthths" (
-	"afm"	VARCHAR(255),
-	"first_name"	VARCHAR(255),
-	"last_name"	VARCHAR(255),
-	"is_pwlhths"	BOOL,
+	"afm" TEXT,
+	"first_name" TEXT,
+	"last_name"	TEXT,
 	PRIMARY KEY("afm")
 );
 CREATE TABLE IF NOT EXISTS "idiokthths_exei" (
-	"akinito_id"	INTEGER,
-	"idiokthths_afm"	VARCHAR(255),
+	"akinito_id" INTEGER,
+	"idiokthths_afm" TEXT,
 	PRIMARY KEY("akinito_id","idiokthths_afm"),
-	FOREIGN KEY("idiokthths_afm") REFERENCES "idiokthths"("afm"),
-	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id")
+	FOREIGN KEY("idiokthths_afm") REFERENCES "idiokthths"("afm") ON DELETE CASCADE, 
+	FOREIGN KEY("akinito_id") REFERENCES "akinhto"("akinito_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "m_endiaferomenos" (
 	"melos_id"	INTEGER,
 	PRIMARY KEY("melos_id" AUTOINCREMENT),
-	FOREIGN KEY("melos_id") REFERENCES "melos"("melos_id")
+	FOREIGN KEY("melos_id") REFERENCES "melos"("melos_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "m_pwlhths" (
-	"melos_id"	INTEGER,
-	"afm"	VARCHAR(255) UNIQUE,
-	"telephone"	VARCHAR(255),
-	"is_mesiths"	BOOL,
+	"melos_id" INTEGER,
+	"afm" INTEGER UNIQUE,
+	"telephone"	TEXT,
+	"is_mesiths" REAL,
 	PRIMARY KEY("melos_id"),
-	FOREIGN KEY("melos_id") REFERENCES "melos"("melos_id")
+	FOREIGN KEY("melos_id") REFERENCES "melos"("melos_id") ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS "mesiths" (
-	"afm"	VARCHAR(255),
-	"brand_name"	VARCHAR(255),
-	"brand_address"	VARCHAR(255),
+	"afm" TEXT,
+	"brand_name" TEXT,
+	"brand_address" TEXT,
 	PRIMARY KEY("afm"),
-	FOREIGN KEY("afm") REFERENCES "m_pwlhths"("melos_id")
+	FOREIGN KEY("afm") REFERENCES "m_pwlhths"("melos_id") ON DELETE CASCADE
 );
 COMMIT;
