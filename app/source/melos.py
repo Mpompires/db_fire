f0ff833fa5114bf58ad3353a34b5f236592de507
ignekt <ignekt@gmail.com>
2021-01-03 06:25:43 +0200

    Update insert_melos to use connection

--- app/source/melos.py
+++ app/source/melos.py
@@ -1,9 +1,9 @@
 from hashlib import sha256
 import sqlite3
+from connect import connect
 
 def insert_melos(melos):
-    con = sqlite3.connect("db.db")
-    cur = con.cursor()
+    con, cur = connect()
     cur.execute(f"INSERT INTO melos VALUES (?,?,?,?,?,?,?,?)", melos)
     con.commit()
     con.close()
