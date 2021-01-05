e74bed86416ab2b59a1a7408352c35cda86922e3
ignekt <ignekt@gmail.com>
2021-01-05 21:15:30 +0200

    Update entering data

--- app/source/enter_data.py
+++ app/source/enter_data.py
@@ -1,16 +1,27 @@
 import csv
 import connect
+from hashlib import sha256
 
 con, cur = connect.connect()
 
 def enter_data_for(entity):
     data = open(f"data/{entity[0]}.csv")
     rows = csv.reader(data)
-    cols = '(?' + ',?' * (entity[1]-1) + ')'
-    cur.executemany(f"INSERT INTO {entity[0]} VALUES {cols}", rows)
+    if entity[0] == "melos":
+        rows = [row for row in rows]
+        for row in rows:
+            row[2] = sha256(row[2].encode('utf-8')).digest()
+            cols = '(?' + ',?' * (entity[1]-1) + ')'
+            cur.execute(f"INSERT INTO melos VALUES {cols}", row)
+    else:
+        cols = '(?' + ',?' * (entity[1]-1) + ')'
+        for row in rows:
+            cur.execute(f"INSERT INTO {entity[0]} VALUES {cols}", row)
 
 entities = [
-    ("akinhto", 6),
+    ("melos", 8),
+    ("m_pwlhths",4),
+    ("akinhto", 7),
     ("a_katoikia", 8),
     ("a_epaggelmatikos_xwros", 5),
     ("a_gh", 3)
